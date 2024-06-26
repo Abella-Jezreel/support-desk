91bc91ee6c9b08dd4fb1b1fd5c72d9c0b5a940ac
Abella <jezreel.abella@accenture.com>
2024-06-26 14:26:52 +0800

    Set up server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Server... yes')
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Support Desk API",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
